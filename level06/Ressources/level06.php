b0d4b24b64214eb08c1a0c8b1f7f192b1859a4ec
xemxav <xavier.emmanuel.moreau@gmail.com>
2020-04-03 10:24:55 -0400

    level06 done

--- level06/Ressources/level06.php
+++ level06/Ressources/level06.php
@@ -1,6 +1,20 @@
 #!/usr/bin/php
 <?php
-function y($m) { $m = preg_replace("/\./", " x ", $m); $m = preg_replace("/@/", " y", $m); return $m; }
-function x($y, $z) { $a = file_get_contents($y); $a = preg_replace("/(\[x (.*)\])/e", "y(\"\\2\")", $a); $a = preg_replace("/\[/", "(", $a); $a = preg_replace("/\]/", ")", $a); return $a; }
-$r = x($argv[1], $argv[2]); print $r;
+function y($m) 
+{ 
+	$m = preg_replace("/\./", " x ", $m); # remplace les points par des " x "
+	$m = preg_replace("/@/", " y", $m); #remplace les @ par " y "
+	return $m; 	
+}
+
+function x($y, $z) 
+{ 
+	$a = file_get_contents($y); # recupere le file de argv[1]
+	$a = preg_replace("/(\[x (.*)\])/e", "y(\"\\2\")", $a); #second groupe : tout ce qui est entre [x et ] et  applique y() au gorupe.
+	$a = preg_replace("/\[/", "(", $a); # va remplacer tous les [ par des (
+	$a = preg_replace("/\]/", ")", $a); # va remplacer des ] par des )
+	return $a; 
+}
+$r = x($argv[1], $argv[2]); 
+print $r;
 ?>
